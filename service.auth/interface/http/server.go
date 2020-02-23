ac668da1576881fb152f4bda101335adfa25ec24
Reece Russell <reece@prominentmedia.com>
2020-02-23 13:51:58 +0000

    Created a user role controller

---
+++ service.auth/interface/http/server.go
@@ -0,0 +1,25 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/reecerussell/monzo-plus-plus/libraries/bootstrap"
+	"github.com/reecerussell/monzo-plus-plus/libraries/di"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/interface/http/controller"
+)
+
+func Build(ctn *di.Container) *bootstrap.HTTPServer {
+	r := mux.NewRouter()
+
+	_ = controller.NewTokenController(ctn, r)
+	_ = controller.NewUserController(ctn, r)
+	_ = controller.NewUserRoleController(ctn, r)
+
+	return bootstrap.BuildServer(&http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf(":%s", os.Getenv("HTTP_PORT")),
+	})
+}
