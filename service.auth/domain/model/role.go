56e8d5bb89d110f178824aa5ed80164cd935077e
Reece Russell <reece@prominentmedia.com>
2020-02-23 19:24:42 +0000

    implemented role usecase

--- service.auth/domain/model/role.go
+++ service.auth/domain/model/role.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"github.com/google/uuid"
+	"github.com/reecerussell/monzo-plus-plus/libraries/errors"
 	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/datamodel"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/dto"
 )
 
 type Role struct {
@@ -9,8 +12,53 @@ type Role struct {
 	name string
 }
 
+func NewRole(d *dto.CreateRole) (*Role, errors.Error) {
+	id, _ := uuid.NewRandom()
+	r := new(Role)
+
+	r.id = id.String()
+
+	err := r.UpdateName(d.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (r *Role) Update(d *dto.Role) errors.Error {
+	err := r.UpdateName(d.Name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Role) UpdateName(name string) errors.Error {
+	l := len(name)
+	if l < 1 {
+		return errors.BadRequest("name cannot be empty")
+	}
+
+	if l > 25 {
+		return errors.BadRequest("name cannot be greater than 25 characters")
+	}
+
+	r.name = name
+
+	return nil
+}
+
 func (r *Role) DataModel() *datamodel.Role {
 	return &datamodel.Role{
+		ID:   r.id,
+		Name: r.name,
+	}
+}
+
+func (r *Role) DTO() *dto.Role {
+	return &dto.Role{
 		ID:   r.id,
 		Name: r.name,
 	}
--- service.auth/usecase/role_usecase.go
+++ service.auth/usecase/role_usecase.go
@@ -1,11 +1,133 @@
 package usecase
 
 import (
+	"context"
+
 	"github.com/reecerussell/monzo-plus-plus/libraries/errors"
 	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/dto"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/model"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/repository"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/domain/service"
+	"github.com/reecerussell/monzo-plus-plus/service.auth/permission"
 )
 
 type RoleUsecase interface {
-	Get(id string) (*dto.Role, errors.Error)
-	GetList(term string) ([]*dto.Role, errors.Error)
+	Get(ctx context.Context, id string) (*dto.Role, errors.Error)
+	GetList(ctx context.Context, term string) ([]*dto.Role, errors.Error)
+	Create(ctx context.Context, d *dto.CreateRole) (*dto.Role, errors.Error)
+	Update(ctx context.Context, d *dto.Role) errors.Error
+	Delete(ctx context.Context, id string) errors.Error
+}
+
+type roleUsecase struct {
+	repo repository.RoleRepository
+	serv *service.RoleService
+}
+
+func NewRoleService(repo repository.RoleRepository, serv *service.RoleService) RoleUsecase {
+	return &roleUsecase{
+		repo: repo,
+		serv: serv,
+	}
+}
+
+func (ru *roleUsecase) Get(ctx context.Context, id string) (*dto.Role, errors.Error) {
+	if !permission.Has(ctx, permission.PermissionGetRole) {
+		return nil, errors.Forbidden()
+	}
+
+	role, err := ru.repo.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return role.DTO(), nil
+}
+
+func (ru *roleUsecase) GetList(ctx context.Context, term string) ([]*dto.Role, errors.Error) {
+	if !permission.Has(ctx, permission.PermissionGetRoleList) {
+		return nil, errors.Forbidden()
+	}
+
+	roles, err := ru.repo.GetList(term)
+	if err != nil {
+		return nil, err
+	}
+
+	dtos := make([]*dto.Role, len(roles))
+
+	for i, r := range roles {
+		dtos[i] = r.DTO()
+	}
+
+	return dtos, nil
+}
+
+func (ru *roleUsecase) Create(ctx context.Context, d *dto.CreateRole) (*dto.Role, errors.Error) {
+	if !permission.Has(ctx, permission.PermissionCreateRole) {
+		return nil, errors.Forbidden()
+	}
+
+	r, err := model.NewRole(d)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ru.serv.ValidateName(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ru.repo.Insert(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.DTO(), nil
+}
+
+func (ru *roleUsecase) Update(ctx context.Context, d *dto.Role) errors.Error {
+	if !permission.Has(ctx, permission.PermissionUpdateRole) {
+		return errors.Forbidden()
+	}
+
+	r, err := ru.repo.Get(d.ID)
+	if err != nil {
+		return err
+	}
+
+	err = r.Update(d)
+	if err != nil {
+		return err
+	}
+
+	err = ru.serv.ValidateName(r)
+	if err != nil {
+		return err
+	}
+
+	err = ru.repo.Update(r)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ru *roleUsecase) Delete(ctx context.Context, id string) errors.Error {
+	if !permission.Has(ctx, permission.PermissionDeleteRole) {
+		return errors.Forbidden()
+	}
+
+	err := ru.repo.EnsureExists(id)
+	if err != nil {
+		return err
+	}
+
+	err = ru.repo.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
