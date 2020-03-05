1bc6c0ab6e5318dbaa97700faffcf67c14104093
Reece Russell <reece@prominentmedia.com>
2020-03-05 17:34:25 +0000

    Reformatted the user menus

--- ui/src/components/layout/header.js
+++ ui/src/components/layout/header.js
@@ -3,8 +3,12 @@ import { Container, Dropdown, Image, Menu, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import * as User from "../../utils/user";
 
-const Header = () => {
-	const adminMenu = User.IsInRole("Admin") ? (
+const AdminMenu = () => {
+	if (!User.IsInRole("Admin")) {
+		return null;
+	}
+
+	return (
 		<>
 			<Dropdown.Divider />
 			<Dropdown.Header>Admin</Dropdown.Header>
@@ -21,10 +25,37 @@ const Header = () => {
 					</Dropdown.Item>
 				</Dropdown.Menu>
 			</Dropdown.Item>
-			<Dropdown.Item>Roles</Dropdown.Item>{" "}
+			<Dropdown.Item>Roles</Dropdown.Item>
 		</>
-	) : null;
+	);
+};
 
+const UserMenu = () => {
+	if (!User.IsAuthenticated()) {
+		return null;
+	}
+
+	return (
+		<Dropdown
+			item
+			simple
+			text={
+				<>
+					<Icon name="user" />
+					{User.GetUsername()}
+				</>
+			}
+		>
+			<Dropdown.Menu>
+				<Dropdown.Item>My Account</Dropdown.Item>
+				<Dropdown.Item>Logout</Dropdown.Item>
+				<AdminMenu />
+			</Dropdown.Menu>
+		</Dropdown>
+	);
+};
+
+const Header = () => {
 	return (
 		<Menu fixed="top" inverted>
 			<Container>
@@ -38,22 +69,7 @@ const Header = () => {
 				</Menu.Item>
 				<Menu.Item as="a">Home</Menu.Item>
 
-				<Dropdown
-					item
-					simple
-					text={
-						<>
-							<Icon name="user" />
-							{User.GetUsername()}
-						</>
-					}
-				>
-					<Dropdown.Menu>
-						<Dropdown.Item>My Account</Dropdown.Item>
-						<Dropdown.Item>Logout</Dropdown.Item>
-						{adminMenu}
-					</Dropdown.Menu>
-				</Dropdown>
+				<UserMenu />
 			</Container>
 		</Menu>
 	);
