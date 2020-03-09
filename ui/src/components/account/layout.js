546c2ff22fd244e3fafdc8ba633fb302e4eae68c
Reece Russell <reece@prominentmedia.com>
2020-03-09 10:03:29 +0000

    Updated account layout

--- ui/src/components/account/layout.js
+++ ui/src/components/account/layout.js
@@ -1,48 +1,12 @@
 import React, { useState, useEffect, createRef } from "react";
-import { Menu, Responsive, Sticky, Ref, Grid } from "semantic-ui-react";
+import { Menu, Sticky, Ref, Grid, Header, Divider } from "semantic-ui-react";
 import { Redirect, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
 	const { pathname } = useLocation();
 	const [activePath, setPath] = useState(pathname);
 	const [redirect, setRedirect] = useState(null);
-	const [menuWidth, setMenuWidth] = useState(16);
 	const contextRef = createRef();
-
-	const handleResponsiveUpdate = () => {
-		const width = window.innerWidth;
-
-		if (
-			width <= Responsive.onlyWidescreen.maxWidth &&
-			width >= Responsive.onlyWidescreen.minWidth
-		) {
-			setMenuWidth(3);
-		}
-		if (
-			width <= Responsive.onlyLargeScreen.maxWidth &&
-			width >= Responsive.onlyLargeScreen.minWidth
-		) {
-			setMenuWidth(4);
-		}
-		if (
-			width <= Responsive.onlyComputer.maxWidth &&
-			width >= Responsive.onlyComputer.minWidth
-		) {
-			setMenuWidth(5);
-		}
-		if (
-			width <= Responsive.onlyTablet.maxWidth &&
-			width >= Responsive.onlyTablet.minWidth
-		) {
-			setMenuWidth(5);
-		}
-		if (
-			width <= Responsive.onlyMobile.maxWidth &&
-			width >= Responsive.onlyMobile.minWidth
-		) {
-			setMenuWidth(16);
-		}
-	};
 
 	useEffect(() => {
 		if (activePath === pathname) {
@@ -52,22 +16,20 @@ const Layout = ({ children }) => {
 		}
 	}, [activePath]);
 
-	useEffect(() => {
-		handleResponsiveUpdate();
-	}, []);
-
 	if (redirect) {
 		return <Redirect to={redirect} />;
 	}
 
 	return (
-		<Responsive onUpdate={handleResponsiveUpdate}>
-			<Grid>
+		<>
+			<Header as="h1">Manage your account</Header>
+			<Divider />
+			<Grid stackable>
 				<Grid.Row>
-					<Grid.Column width={menuWidth}>
+					<Grid.Column width={4}>
 						<Ref innerRef={contextRef}>
 							<Sticky context={contextRef}>
-								<Menu pointing vertical fluid>
+								<Menu vertical fluid>
 									<Menu.Item
 										name={"Details"}
 										active={activePath === "/account"}
@@ -106,12 +68,10 @@ const Layout = ({ children }) => {
 							</Sticky>
 						</Ref>
 					</Grid.Column>
-					<Grid.Column width={menuWidth === 16 ? 16 : 16 - menuWidth}>
-						{children}
-					</Grid.Column>
+					<Grid.Column width={12}>{children}</Grid.Column>
 				</Grid.Row>
 			</Grid>
-		</Responsive>
+		</>
 	);
 };
 
