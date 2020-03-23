8bda53eaa17cadf3e089ef60bae6f4e4b00b9747
Reece Russell <reece@prominentmedia.com>
2020-03-23 10:36:34 +0000

    Added permission middleware

---
+++ libraries/permission/middleware.go
@@ -0,0 +1,76 @@
+package permission
+
+import (
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/reecerussell/monzo-plus-plus/libraries/errors"
+)
+
+var (
+	mu   = sync.RWMutex{}
+	urls = []string{}
+
+	errNoAuthHeader          = errors.Unauthorised("no authorization header")
+	errMalformedAuthHeader   = errors.Unauthorised("malformed authorization header")
+	errUnsupportedAuthScheme = errors.Unauthorised("unsupported authorization scheme")
+)
+
+// IgnoreURL adds a string to the ignore list.
+func IgnoreURL(substr string) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	urls = append(urls, substr)
+}
+
+// Middleware provides an authentication middleware method to ensure a user
+// has the givenn permission.
+func Middleware(h http.Handler, perm int) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isIgnored(r.URL.Path) {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			errors.HandleHTTPError(w, r, errNoAuthHeader)
+			return
+		}
+
+		p := strings.Split(auth, " ")
+		if len(p) < 2 {
+			errors.HandleHTTPError(w, r, errMalformedAuthHeader)
+			return
+		}
+
+		if p[0] != "Bearer" {
+			errors.HandleHTTPError(w, r, errUnsupportedAuthScheme)
+			return
+		}
+
+		if !Has(p[1], perm) {
+			errors.HandleHTTPError(w, r, errors.Forbidden())
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
+func isIgnored(path string) bool {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, url := range urls {
+		if strings.Contains(
+			strings.ToLower(path),
+			strings.ToLower(url)) {
+			return true
+		}
+	}
+
+	return false
+}
