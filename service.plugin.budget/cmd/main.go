82501046d3fb65fba9d2983f9e2021af311a7fa0
Reece Russell <reece@prominentmedia.com>
2020-03-23 10:45:47 +0000

    Implemented entypoint

--- service.plugin.budget/cmd/main.go
+++ service.plugin.budget/cmd/main.go
@@ -4,14 +4,16 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/reecerussell/monzo-plus-plus/service.plugin.budget/interface/http"
-
 	"github.com/reecerussell/monzo-plus-plus/libraries/bootstrap"
+	"github.com/reecerussell/monzo-plus-plus/service.plugin.budget/interface/http"
 	"github.com/reecerussell/monzo-plus-plus/service.plugin.budget/interface/rpc"
 	"github.com/reecerussell/monzo-plus-plus/service.plugin.budget/registry"
 )
 
 func main() {
+	bootstrap.Register(os.Getenv("NAME"), os.Getenv("HOSTNAME"))
+	defer bootstrap.Unregister(os.Getenv("NAME"))
+
 	ctn := registry.Build()
 
 	web := http.New(ctn)
