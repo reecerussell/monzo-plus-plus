f6414ce99dce5921c55a7669bf7c650d532a629a
Reece Russell <reece@prominentmedia.com>
2020-03-31 14:36:36 +0100

    Layout changes

--- ui/src/views/account/plugins.js
+++ ui/src/views/account/plugins.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Grid } from "semantic-ui-react";
 import { Authorise } from "../../containers/login";
 import Layout from "../../components/account/layout";
 import PluginsContainer from "../../containers/account/plugins";
@@ -10,11 +10,20 @@ const Plugins = () => {
 			<Layout>
 				<Header as="h2">Plugins</Header>
 
-				<p>
-					Plugins are individual features that you can add to your
-					account. You can add as many as you like, simpily by
-					enabling one below.
-				</p>
+				<Grid stackable>
+					<Grid.Column width="12">
+						<p>
+							Plugins are individual features that you can add to
+							your account. You can add as many as you like,
+							simpily by enabling one below.
+						</p>
+						<p>
+							If you ever decide you'd like to stop one, either
+							temporarily or permanently, you can click the
+							"Disable Plugin" button in the section below.
+						</p>
+					</Grid.Column>
+				</Grid>
 
 				<PluginsContainer />
 			</Layout>
