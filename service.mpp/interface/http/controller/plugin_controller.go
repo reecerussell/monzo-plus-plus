ad883c647595cccb03c0e5367d9b9953a6cec6a7
Reece Russell <reece@prominentmedia.com>
2020-04-07 20:38:51 +0100

    Refactored the plugin controller

--- service.mpp/interface/http/controller/plugin_controller.go
+++ service.mpp/interface/http/controller/plugin_controller.go
@@ -1,11 +1,16 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/reecerussell/monzo-plus-plus/libraries/bootstrap"
+	"github.com/reecerussell/monzo-plus-plus/libraries/errors"
 )
 
 // Environment variables.
@@ -13,14 +18,58 @@ var (
 	PluginsHTTPHost = os.Getenv("PLUGINS_HTTP_HOST")
 )
 
-// PluginProxy handles HTTP requests to /api/plugins and forwards the requests to the plugins api.
-func PluginProxy() http.HandlerFunc {
-	remote, _ := url.Parse(PluginsHTTPHost)
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+type PluginController struct {
+	hosts   map[string]*httputil.ReverseProxy
+	plugins *httputil.ReverseProxy
+}
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.URL.Path = strings.Replace(r.URL.Path, "/api/plugins", "", 1)
+func NewPluginController(r *mux.Router) *PluginController {
+	pluginsURL, _ := url.Parse(PluginsHTTPHost)
 
-		proxy.ServeHTTP(w, r)
-	})
+	c := &PluginController{
+		hosts:   make(map[string]*httputil.ReverseProxy),
+		plugins: httputil.NewSingleHostReverseProxy(pluginsURL),
+	}
+
+	r.HandleFunc("/api/plugins/", c.HandlePlugins)
+	r.HandleFunc("/api/plguin/{name}", c.HandlePluginAPI)
+
+	return c
+}
+
+// HandlePlugins handles HTTP requests to /api/plugins and forwards the requests to the plugins api.
+func (c *PluginController) HandlePlugins(w http.ResponseWriter, r *http.Request) {
+	r.URL.Path = strings.Replace(r.URL.Path, "/api/plugins", "", 1)
+
+	c.plugins.ServeHTTP(w, r)
+}
+
+// HandlePluginAPI acts as a reverse proxy to internal plugin APIs.
+func (c *PluginController) HandlePluginAPI(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+	if name == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	proxy, ok := c.hosts[name]
+	if !ok {
+		host, err := bootstrap.GetHost(name)
+		if err != nil {
+			errors.HandleHTTPError(w, r, errors.InternalError(err))
+			return
+		}
+
+		if host == "" {
+			http.NotFound(w, r)
+			return
+		}
+
+		url, _ := url.Parse(host)
+		c.hosts[name] = httputil.NewSingleHostReverseProxy(url)
+		proxy = c.hosts[name]
+	}
+
+	r.URL.Path = strings.Replace(r.URL.Path, fmt.Sprintf("/api/plugin/%s", name), "", 1)
+	proxy.ServeHTTP(w, r)
 }
